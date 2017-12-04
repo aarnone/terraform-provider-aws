25e29cd603ac7359d2a3295547d5d4138f37596d
Alessandro Arnone <arnone.alessandro@gmail.com>
2017-12-04 00:40:15 +0000

    Implement migration from v1 to v2

--- aws/resource_aws_kinesis_firehose_delivery_stream_migrate.go
+++ aws/resource_aws_kinesis_firehose_delivery_stream_migrate.go
@@ -3,6 +3,8 @@ package aws
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -11,8 +13,16 @@ func resourceAwsKinesisFirehoseMigrateState(
 	v int, is *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
 	switch v {
 	case 0:
-		log.Println("[INFO] Found AWS Kinesis Firehose Delivery Stream State v0; migrating to v1")
-		return migrateKinesisFirehoseV0toV1(is)
+		log.Println("[INFO] Found AWS Kinesis Firehose Delivery Stream State v0; migrating to v2")
+		newIs, err := migrateKinesisFirehoseV0toV1(is)
+		if err != nil {
+			return nil, err
+		}
+
+		return migrateKinesisFirehoseV1toV2(newIs)
+	case 1:
+		log.Println("[INFO] Found AWS Kinesis Firehose Delivery Stream State v1; migrating to v2")
+		return migrateKinesisFirehoseV1toV2(is)
 	default:
 		return is, fmt.Errorf("Unexpected schema version: %d", v)
 	}
@@ -24,7 +34,7 @@ func migrateKinesisFirehoseV0toV1(is *terraform.InstanceState) (*terraform.Insta
 		return is, nil
 	}
 
-	log.Printf("[DEBUG] Attributes before migration: %#v", is.Attributes)
+	log.Printf("[DEBUG] Attributes before migration from V0 to V1: %#v", is.Attributes)
 
 	// migrate flate S3 configuration to a s3_configuration block
 	// grab initial values
@@ -54,6 +64,63 @@ func migrateKinesisFirehoseV0toV1(is *terraform.InstanceState) (*terraform.Insta
 	delete(is.Attributes, "s3_buffer_interval")
 	delete(is.Attributes, "s3_prefix")
 
-	log.Printf("[DEBUG] Attributes after migration: %#v", is.Attributes)
+	log.Printf("[DEBUG] Attributes after migration from V0 to V1: %#v", is.Attributes)
+	return is, nil
+}
+
+func migrateKinesisFirehoseV1toV2(is *terraform.InstanceState) (*terraform.InstanceState, error) {
+	if is.Empty() {
+		log.Println("[DEBUG] Empty Kinesis Firehose Delivery State; nothing to migrate.")
+		return is, nil
+	}
+
+	log.Printf("[DEBUG] Attributes before migration from V1 to V2: %#v", is.Attributes)
+
+	// basic s3_configuration will be deprecated, changes are now reflected in extended_s3_configuration
+	// keep everything under extended_s3_configuration to avoid confusion
+
+	for key, value := range is.Attributes {
+		if strings.HasPrefix(key, "s3_configuration") {
+			newKey := strings.Replace(key, "s3_configuration", "extended_s3_configuration", 1)
+			is.Attributes[newKey] = value
+			delete(is.Attributes, key)
+		}
+	}
+
+	// processor parameters are now stored individually
+	// convert parameter_name/parameter_value pairs
+	paramNameRe := regexp.MustCompile(`^((extended_s3_configuration\.\d+\.processing_configuration\.\d+\.processors\.\d+\.parameters\.)\d+\.)parameter_name$`)
+	for key, value := range is.Attributes {
+		if parsed := paramNameRe.FindStringSubmatch(key); len(parsed) == 3 {
+			prefix, prefixWithIndex, parameterName := parsed[2], parsed[1], value
+
+			var newKey string
+			switch parameterName {
+			case "LambdaArn":
+				newKey = prefix + "lambda_arn"
+			case "NumberOfRetries":
+				newKey = prefix + "number_of_retries"
+			default:
+				return nil, fmt.Errorf("[ERROR] Unexpected parameter name for processor: %v", parameterName)
+			}
+
+			valueKey := prefixWithIndex + "parameter_value"
+			is.Attributes[newKey] = is.Attributes[valueKey]
+			delete(is.Attributes, key)
+			delete(is.Attributes, valueKey)
+		}
+	}
+
+	// cleanup orphan values and list counter
+	paramValueRe := regexp.MustCompile(`^extended_s3_configuration\.\d+\.processing_configuration\.\d+\.processors\.\d+\.parameters\.\d+\.parameter_value$`)
+	counterRe := regexp.MustCompile(`^extended_s3_configuration\.\d+\.processing_configuration\.\d+\.processors\.\d+\.parameters\.#$`)
+	for key := range is.Attributes {
+		if counterRe.MatchString(key) ||
+			paramValueRe.MatchString(key) {
+			delete(is.Attributes, key)
+		}
+	}
+
+	log.Printf("[DEBUG] Attributes after migration from V1 to V2: %#v", is.Attributes)
 	return is, nil
 }
--- aws/resource_aws_kinesis_firehose_delivery_stream_migrate_test.go
+++ aws/resource_aws_kinesis_firehose_delivery_stream_migrate_test.go
@@ -13,7 +13,7 @@ func TestAWSKinesisFirehoseMigrateState(t *testing.T) {
 		Expected     map[string]string
 		Meta         interface{}
 	}{
-		"v0.6.16 and earlier": {
+		"v0 to v2": {
 			StateVersion: 0,
 			Attributes: map[string]string{
 				// EBS
@@ -24,15 +24,15 @@ func TestAWSKinesisFirehoseMigrateState(t *testing.T) {
 				"s3_data_compression": "GZIP",
 			},
 			Expected: map[string]string{
-				"s3_configuration.#":                    "1",
-				"s3_configuration.0.bucket_arn":         "arn:aws:s3:::tf-test-bucket",
-				"s3_configuration.0.buffer_interval":    "400",
-				"s3_configuration.0.buffer_size":        "10",
-				"s3_configuration.0.compression_format": "GZIP",
-				"s3_configuration.0.role_arn":           "arn:aws:iam::somenumber:role/tf_acctest_4271506651559170635",
+				"extended_s3_configuration.#":                    "1",
+				"extended_s3_configuration.0.bucket_arn":         "arn:aws:s3:::tf-test-bucket",
+				"extended_s3_configuration.0.buffer_interval":    "400",
+				"extended_s3_configuration.0.buffer_size":        "10",
+				"extended_s3_configuration.0.compression_format": "GZIP",
+				"extended_s3_configuration.0.role_arn":           "arn:aws:iam::somenumber:role/tf_acctest_4271506651559170635",
 			},
 		},
-		"v0.6.16 and earlier, sparse": {
+		"v0 to v2, partial": {
 			StateVersion: 0,
 			Attributes: map[string]string{
 				// EBS
@@ -40,10 +40,90 @@ func TestAWSKinesisFirehoseMigrateState(t *testing.T) {
 				"s3_bucket_arn": "arn:aws:s3:::tf-test-bucket",
 			},
 			Expected: map[string]string{
-				"s3_configuration.#":            "1",
-				"s3_configuration.0.bucket_arn": "arn:aws:s3:::tf-test-bucket",
-				"s3_configuration.0.role_arn":   "arn:aws:iam::somenumber:role/tf_acctest_4271506651559170635",
+				"extended_s3_configuration.#":            "1",
+				"extended_s3_configuration.0.bucket_arn": "arn:aws:s3:::tf-test-bucket",
+				"extended_s3_configuration.0.role_arn":   "arn:aws:iam::somenumber:role/tf_acctest_4271506651559170635",
 			},
+		},
+		"v1 to v2": {
+			StateVersion: 1,
+			Attributes: map[string]string{
+				"destination":                                                              "s3",
+				"s3_configuration.#":                                                       "1",
+				"s3_configuration.0.bucket_arn":                                            "arn:aws:s3:::tf-test-bucket-7748474670850130673",
+				"s3_configuration.0.buffer_interval":                                       "300",
+				"s3_configuration.0.buffer_size":                                           "5",
+				"s3_configuration.0.cloudwatch_logging_options.#":                          "1",
+				"s3_configuration.0.cloudwatch_logging_options.2513562885.enabled":         "true",
+				"s3_configuration.0.cloudwatch_logging_options.2513562885.log_group_name":  "groupname",
+				"s3_configuration.0.cloudwatch_logging_options.2513562885.log_stream_name": "streamname",
+				"s3_configuration.0.compression_format":                                    "UNCOMPRESSED",
+				"s3_configuration.0.kms_key_arn":                                           "kmskeyarn",
+				"s3_configuration.0.prefix":                                                "s3prefix",
+				"s3_configuration.0.role_arn":                                              "arn:aws:iam::666:role/tf_acctest_firehose_delivery_role_7748474670850130673",
+			},
+			Expected: map[string]string{
+				"destination":                                                                       "s3",
+				"extended_s3_configuration.#":                                                       "1",
+				"extended_s3_configuration.0.bucket_arn":                                            "arn:aws:s3:::tf-test-bucket-7748474670850130673",
+				"extended_s3_configuration.0.buffer_interval":                                       "300",
+				"extended_s3_configuration.0.buffer_size":                                           "5",
+				"extended_s3_configuration.0.cloudwatch_logging_options.#":                          "1",
+				"extended_s3_configuration.0.cloudwatch_logging_options.2513562885.enabled":         "true",
+				"extended_s3_configuration.0.cloudwatch_logging_options.2513562885.log_group_name":  "groupname",
+				"extended_s3_configuration.0.cloudwatch_logging_options.2513562885.log_stream_name": "streamname",
+				"extended_s3_configuration.0.compression_format":                                    "UNCOMPRESSED",
+				"extended_s3_configuration.0.kms_key_arn":                                           "kmskeyarn",
+				"extended_s3_configuration.0.prefix":                                                "s3prefix",
+				"extended_s3_configuration.0.role_arn":                                              "arn:aws:iam::666:role/tf_acctest_firehose_delivery_role_7748474670850130673",
+			},
+		},
+		"v1 to v2, processors": {
+			StateVersion: 1,
+			Attributes: map[string]string{
+				"extended_s3_configuration.#":                                                                      "1",
+				"extended_s3_configuration.0.bucket_arn":                                                           "arn:aws:s3:::tf-test-bucket-4747185083350722827",
+				"extended_s3_configuration.0.buffer_interval":                                                      "300",
+				"extended_s3_configuration.0.buffer_size":                                                          "5",
+				"extended_s3_configuration.0.compression_format":                                                   "UNCOMPRESSED",
+				"extended_s3_configuration.0.kms_key_arn":                                                          "kmskeyarn",
+				"extended_s3_configuration.0.prefix":                                                               "s3prefix",
+				"extended_s3_configuration.0.processing_configuration.#":                                           "1",
+				"extended_s3_configuration.0.processing_configuration.0.enabled":                                   "false",
+				"extended_s3_configuration.0.processing_configuration.0.processors.#":                              "1",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.#":                 "2",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.0.parameter_name":  "LambdaArn",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.0.parameter_value": "arn:aws:lambda:us-west-2:666:function:aws_kinesis_firehose_delivery_stream_test_rvab6",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.1.parameter_name":  "NumberOfRetries",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.1.parameter_value": "3",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.type":                         "Lambda",
+				"extended_s3_configuration.0.role_arn":                                                             "arn:aws:iam::666:role/tf_acctest_firehose_delivery_role_4747185083350722827",
+			},
+			Expected: map[string]string{
+				"extended_s3_configuration.#":                                                                      "1",
+				"extended_s3_configuration.0.bucket_arn":                                                           "arn:aws:s3:::tf-test-bucket-4747185083350722827",
+				"extended_s3_configuration.0.buffer_interval":                                                      "300",
+				"extended_s3_configuration.0.buffer_size":                                                          "5",
+				"extended_s3_configuration.0.compression_format":                                                   "UNCOMPRESSED",
+				"extended_s3_configuration.0.kms_key_arn":                                                          "kmskeyarn",
+				"extended_s3_configuration.0.prefix":                                                               "s3prefix",
+				"extended_s3_configuration.0.processing_configuration.#":                                           "1",
+				"extended_s3_configuration.0.processing_configuration.0.enabled":                                   "false",
+				"extended_s3_configuration.0.processing_configuration.0.processors.#":                              "1",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.lambda_arn":        "arn:aws:lambda:us-west-2:666:function:aws_kinesis_firehose_delivery_stream_test_rvab6",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.parameters.number_of_retries": "3",
+				"extended_s3_configuration.0.processing_configuration.0.processors.0.type":                         "Lambda",
+				"extended_s3_configuration.0.role_arn":                                                             "arn:aws:iam::666:role/tf_acctest_firehose_delivery_role_4747185083350722827",
+			},
+		},
+		"v1 to v2, orphans": {
+			StateVersion: 1,
+			Attributes: map[string]string{
+				"extended_s3_configuration.0.processing_configuration.#":                                           "5",
+				"extended_s3_configuration.0.processing_configuration.1.processors.0.parameters.0.parameter_value": "arn:aws:lambda:us-west-2:666:function:aws_kinesis_firehose_delivery_stream_test_rvab6",
+				"extended_s3_configuration.0.processing_configuration.2.processors.0.parameters.1.parameter_value": "3",
+			},
+			Expected: map[string]string{},
 		},
 	}
 
